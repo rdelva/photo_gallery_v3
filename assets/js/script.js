16a91cc40a81b501dd2b0908c09320f906343747
Rose <roseldelva@gmail.com>
2018-07-11 00:59:19 -0400

    re do entire code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,21 @@
 
 //Type in word into search bar
+//deleting word  brings up all images
 //search bar takes in value
-//hides all images
-//build gallery List
+//looks at each photo
+// if alt tag has letter or word show it
+// if alt tag doesn't find it hide it
+
 //compare result item to each alt tag
 //if value matches put it in result list
-//print out new list into flex-container
-//if word is in alt tag  put it in a list 
-// hide all items
-// print out list
-//if no item is found dont hide list
-//output new list onto screen
 
 
 
 
 
- $('#search').on('keyup', function(){
+
+
+ $('#search').on('keyup', function(e){
 
 
  	//'
@@ -31,11 +30,11 @@
 
  		 else {
 
- 		 	$(".gallery a").show();
- 		 	$('.gallery').removeClass('flex-start');
- 		 	$('.gallery').addClass('flex-container');
+ 		 	$('.gallery img').show();
 
  		 }
+
+ 	
 
 });
 
@@ -43,84 +42,23 @@
 
 function searchforImage($result){
 
-		var $imageList= $('.gallery a');
-
-		console.log($imageList);
-
-				$(".gallery a").hide();
-		var $searchList = [ ];
 
 
-		//Checks Search List before a value is entered to avoid repeated entries
+		$('.gallery img').each(function(index){
 
-		while($searchList.length > 0){
-					
-				$searchList.pop();
+			var imageAlt = $(this).attr('alt');
 
-		}
+			//search for image
+			var found = imageAlt.search($result);
 
+			if(found === -1 ) {
 
-
-		console.log("Search  List " + $searchList.length);
-
-
-		for(var i = 0; i < $imageList.length; i++ ){
-
-			var $altTag = $imageList[i].innerHTML;
-			var n = $altTag.search($result);
-
-		
-			if(n > -1){ // found something
-				
-				//clears the search list
-				$searchList.push($imageList[i]);
-
+				$(this).hide();
 			} 
-
-
-		}// end of for loop to push images		
-		console.log("Search Built List " + $searchList.length);
-
-
-		printImageList($searchList);
-
-
-}
-
-
-function printImageList($searchList){
-
-		console.log("Printing " + $searchList.length)
 		
 
-
-		if($searchList.length < 4){
-					$('.gallery').removeClass('flex-container');
-					$('.gallery').addClass('flex-start');
-		} else {
-
-					$('.gallery').addClass('flex-container');
-
-		}
-
-		for (var i=0; i < $searchList.length; i++)
-		{	
-			
-			//<a href=""><img src=""></a>
-			var photo = $searchList[i];
-			var link = "<a href='"; 
-			link += photo.href; 
-			link += "'>";
-			link += photo.innerHTML.trim();
-			link += "</a>"
-		
-			$('.gallery').append(link);
-
-	}
-
-	
+		});
 }
-
 
 
   lightbox.option({
